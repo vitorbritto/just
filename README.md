0a95c1232091028cc1dfe7ff21f94c2b0b309c40
Vitor Britto <code@vitorbritto.com.br>
2014-01-23 00:50:24 -0200

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-just
-====
+# Just
+A ridiculously simple task runner.
+
+## Getting Started
+Alright, I know. Another way to perform automation tasks. But it never hurts to have options, right? :)
+
+> NOTE: This is a work in progress and I will likely add more functionality in the future.
+
+### Features
+This build script is capable of processing and minifying stylus files and compressing and minifying JavaScript files.
+
+### Installation
+
+[ 1 ] - First of all, make sure you have [Node.js](http://nodejs.org/) installed.
+[ 2 ] - Clone this repository and create your project folder
+
+```bash
+git clone git://github.com/vitorbritto/just.git projectname
+cd projectname
+```
+
+[ 3 ] - Install dependencies and make the script executable
+
+```bash
+npm install
+chmod u+x just.js
+```
+
+[ 4 ] - Install tasks dependencies: `./just npm` *[!]*
+[ 5 ] - Generate the scaffolding for your project: `./just build` *[!]*
+[ 6 ] - You're ready to go! :)
+
+#### Task Options
+
+- Build: `./just run`
+- Watch: `./just watch`
+
+> [!] - When you run the script for the first time!
+
+### Configuration
+Feel free to change the configuration to be whatever you want. Just modify the `config` properties/methods in `./lib/config.js` file.
+
+> Use `node just --help` for more details and instructions.
+
+## Contributing
+Feel free to [contribute](https://github.com/gruntbrasil/just/pulls) with this project or leave a [suggestion](https://github.com/gruntbrasil/just/issues).
+
+## Acknowledgments
+This script was inspired by [./task.js](https://gist.github.com/substack/8313379) and so many others articles from [dailyjs.com](dailyjs.com), [2ality](2ality.com) and [shapeshed.com](shapeshed.com).
+
+
+## License
+Copyright (c) 2014 Vitor Britto Licensed under the [MIT license](LICENSE).
---
+++ just.js
@@ -0,0 +1,135 @@
+#!/usr/bin/env node
+
+// =====================================================
+// Initial Configuration
+// =====================================================
+
+// Modules
+var exec   = require('child_process').exec,
+    cmd    = require('commander'),
+    cl     = require('colors'),
+    config = require('./lib/config.js'),
+    Just   = require('orchestrator'),
+    just   = new Just();
+
+
+
+// =====================================================
+// CLI Commands
+// =====================================================
+
+// Create Structure
+cmd
+    .command('build')
+    .description('Create a basic Structure')
+    .action(scaffold);
+
+// Install Dependencies
+cmd
+    .command('npm')
+    .description('Install dependencies')
+    .action(install);
+
+// Tasks
+cmd
+    .command('run')
+    .description('Run lint, minify and compile tasks')
+    .action(build);
+
+cmd
+    .command('watch')
+    .description('Run build tasks and watch for changes')
+    .action(watch);
+
+
+// Config
+cmd.parse(process.argv);
+
+if (process.argv.length === 2) {
+    command.help();
+}
+
+
+// =====================================================
+// Create a basic Structure
+// =====================================================
+
+function scaffold() {
+
+    console.log('→ Building'.cyan);
+
+    exec('mkdir -p ' + [config.app_style, config.app_script, config.public_style, config.public_script].join(' '));
+    exec('cp lib/template/style.styl ' + config.app_style);
+    exec('cp lib/template/main.js ' + config.app_script);
+    exec('cp lib/template/style.css ' + config.public_style);
+    exec('cp lib/template/index.html ' + config.public_view);
+
+    console.log('✔ done'.green);
+
+}
+
+
+// =====================================================
+// Install dependencies
+// =====================================================
+
+function install() {
+
+    console.log('→ Installing'.cyan);
+
+    var globals = config.dependencies;
+    globals.forEach(function (mods) {
+        exec('npm -g ' + mods);
+    });
+
+    console.log('✔ done'.green);
+}
+
+
+// =====================================================
+// Build Task
+// =====================================================
+
+function build() {
+
+    console.log('→ Runnning'.cyan);
+    console.log('');
+
+    // Script task
+    just.add('build', function() {
+        config.jshint();
+        config.minify();
+        config.compile();
+        config.template();
+    });
+
+    // Run tasks
+    just.start(['build']);
+}
+
+
+// =====================================================
+// Watch Task
+// =====================================================
+
+function watch() {
+
+    // Watch task
+    just.add('watch', function() {
+        config.refresh();
+    });
+
+    // Watch task must be complete before this one begins
+    just.add('build', ['watch'], function() {
+        config.jshint();
+        config.minify();
+        config.compile();
+    });
+
+    // Run tasks
+    just.start(['watch', 'build']);
+
+    console.log('→ Watching for changes...'.cyan);
+    console.log('');
+    console.log('→ Press CTRL+C to exit'.yellow);
+}
