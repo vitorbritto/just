72508df1a48a31567b7824f107ee2e76d6cb0a38
Vitor Britto <code@vitorbritto.com.br>
2014-04-03 05:06:35 -0300

    Fix make.js

--- make.js
+++ make.js
@@ -12,7 +12,18 @@
 require('shelljs/make');
 require('colors');
 
-var config = require('./lib/config');
+var system  = require('os').platform,
+    isLinux = (system === 'linux'),
+    isMac   = (system === 'darwin');
+
+var config  = {
+    app_view:      './app/',
+    app_style:     './app/css/',
+    app_script:    './app/js/',
+    public_view:   './public/',
+    public_style:  './public/css/',
+    public_script: './public/js/',
+};
 
 
 // =============================================================================
@@ -24,73 +35,83 @@ target.all = function() {
     target.executable();
     target.build();
 
-    echo('');
+    echo('\n');
     echo('✔ All done'.green);
 };
 
 target.npm = function() {
 
     if (!which('csslint')) {
-        exec('npm i csslint -g');
+        echo('\n');
+        echo('→ Installing CSSLint...' .yellow);
+        exec('npm i csslint -gq');
+        echo('✔ CSSLint installed successfully.'.green);
     }
 
     if (!which('jshint')) {
-        exec('npm i jshint -g');
+        echo('\n');
+        echo('→ Installing JSHint...' .yellow);
+        exec('npm i jshint -gq');
+        echo('✔ JSHint installed successfully.'.green);
     }
 
     if (!which('stylus')) {
-        exec('npm i stylus -g');
+        echo('\n');
+        echo('→ Installing Stylus...' .yellow);
+        exec('npm i stylus -gq');
+        echo('✔ Stylus installed successfully.'.green);
     }
 
     if (!which('browser-sync')) {
-        exec('npm i browser-sync -g');
+        echo('\n');
+        echo('→ Installing Browser Sync...' .yellow);
+        exec('npm i browser-sync -gq');
+        echo('✔ Browser Sync installed successfully.'.green);
     }
 
     if (!which('uglifyjs')) {
-        exec('npm i uglify-js -g');
+        echo('\n');
+        echo('→ Installing Uglify JS...' .yellow);
+        exec('npm i uglify-js -gq');
+        echo('✔ Uglify JS installed successfully.'.green);
     }
-
-    echo('✔ Dependencies installed successfully.'.cyan);
 
 };
 
 target.executable = function() {
-    exec('chmod u+x just.js');
-    echo('✔ Script is now executable'.cyan);
+
+    if (isMac || isLinux) {
+        exec('chmod u+x ./just.js');
+        echo('✔ Script is now executable'.cyan);
+    }
+
 };
 
 target.build = function() {
 
     // Create Structure
-    if (!test('-d', config.app_style)) {
-        mkdir('-p', config.app_style);
-    }
-
-    if (!test('-d', config.app_script)) {
-        mkdir('-p', config.app_script);
-    }
-
-    if (!test('-d', config.public_style)) {
-        mkdir('-p', config.public_style);
-    }
-
-    if (!test('-d', config.public_script)) {
-        mkdir('-p', config.public_script);
-    }
+    mkdir('-p', config.app_style);
+    mkdir('-p', config.app_script);
+    mkdir('-p', config.public_style);
+    mkdir('-p', config.public_script);
 
     // Copy files
     cp('-rf', './lib/template/style.styl', config.app_style);
     cp('-rf', './lib/template/main.js', config.app_script);
     cp('-rf', './lib/template/index.html', config.public_view);
-    cp('-rf', './lib/template/.jshintrc', './');
+    cp('-rf', './lib/template/.*rc', './');
     cp('-rf', './lib/tasks.js', './');
     cp('-rf', './lib/config.js', './');
 
     // Removing stuff you don't want
     rm('-rf', ['./.git', './lib' , '.gitignore', 'README.md', '.travis.yml', 'LICENSE']);
 
-    echo('✔ Application source files has been output to: '.cyan + config.app_view);
-    echo('✔ Public files has been output to: '.cyan + config.public_view);
+    echo('\n');
+    echo('----------------------------------------------------------------------');
+    echo('→ Application source files has been output to: '.cyan + config.app_view);
+    echo('→ Public files has been output to: '.cyan + config.public_view);
+    echo('----------------------------------------------------------------------');
+    echo('\n');
 
     // Lastly, avoiding to interrupt the process
     rm('-rf', ['make.js']);
